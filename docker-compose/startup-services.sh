abda88bc4e4214fec177f1495cad04da9a8e99f0
刘德财 <442823529@qq.com>
2022-08-27 15:52:21 +0800

    Update startup-services.sh

--- docker-compose/startup-services.sh
+++ docker-compose/startup-services.sh
@@ -11,8 +11,8 @@ function deploy_docker_compose() {
     docker network create ${network_name} && docker-compose --compatibility up -d --remove-orphans
   fi
 
-  echo "$(date '+%Y-%m-%d %H:%M:%S') ======= 删除没使用且是10天之前的镜像"
-  docker image prune -a --force --filter "until=240h"
+  # echo "$(date '+%Y-%m-%d %H:%M:%S') ======= 删除没使用且是10天之前的镜像"
+  # docker image prune -a --force --filter "until=240h"
 
   echo "$(date '+%Y-%m-%d %H:%M:%S') ======= 磁盘和内存使用情况"
   df -h && free -h
